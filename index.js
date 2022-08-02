698c145ae1e512fc2bcea014b84b8d3e5872e08d
Enow Clinton <enowclinton076@gmail.com>
2022-08-02 23:02:24 +0300

    calculations

---
+++ index.js
@@ -0,0 +1,6 @@
+function Multiply() {
+  const Number1 = parseInt(document.getElementById("Number1").value);
+  const Number2 = parseInt(document.getElementById("Number2").value);
+
+  document.getElementById("result").innerText = Number1 * Number2;
+}
